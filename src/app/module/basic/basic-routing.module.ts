ae6b3a366cd1313a2d6a459b469fd83bf3ad0a1c
ROOT BABU <babuhacker360@gmail.com>
2022-06-15 16:59:53 +0530

    created homepage

---
+++ src/app/module/basic/basic-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasicComponent } from 'src/app/components/basic/basic.component';
+
+const routes: Routes = [
+  {path:'',component:BasicComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BasicRoutingModule { }
